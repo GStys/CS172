062ea797bf0fbcc6d594e198f4d09abc78329504
griffinstyslinger <griffinstyslinger@maclabcs15.lclark.edu>
2016-01-29 14:56:23 -0800

    gku

--- Project2/src/TicTacToe.java
+++ Project2/src/TicTacToe.java
@@ -5,9 +5,14 @@ public class TicTacToe {
 		String[][] board = new String[4][4];
 		for (int i=1;i<4;i++) {
 			for (int j=1;j<4;j++) {
-				board[i][j] = "_";
+				board[i][j] = "[]";
 			}
 		}
+		System.out.println(board[1][1] + " | " + board[1][2] + " | " + board[1][3]);
+		System.out.println("---|----|-----");
+		System.out.println(board[2][1] + " | " + board[2][2] + " | " + board[2][3]);
+		System.out.println("---|----|-----");
+		System.out.println(board[3][1] + " | " + board[3][2] + " | " + board[3][3]);
 
 	}
 
