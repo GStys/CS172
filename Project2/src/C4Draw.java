26494b028410a88f5fad9424473062777fa2ab26
Griffin <griffinstyslinger@gmail.com>
2016-02-10 18:21:23 -0800

    - C4Draw.java is work in progress - added Project3
    Signed-off-by: Griffin <griffinstyslinger@gmail.com>

--- Project2/src/C4Draw.java
+++ Project2/src/C4Draw.java
@@ -8,8 +8,8 @@ public class C4Draw {
 
 	public C4Draw() {
 		board = new String[7][8];
-		for (int i=1;i<7;i++) {
-			for (int j=1;j<8;j++) {
+		for (int i=0;i<7;i++) {
+			for (int j=0;j<8;j++) {
 				board[i][j] = " ";
 			}
 		}
@@ -47,6 +47,7 @@ public class C4Draw {
 		StdDraw.setPenRadius(0.01);
         StdDraw.setPenColor(StdDraw.WHITE);
 		StdDraw.clear(StdDraw.BLACK);
+		StdDraw.text(0.2, 0.8, turn + "'s turn:");
 		for (double i=0.15;i<0.85;i+=0.1) {
 			StdDraw.line(i, 0.1, i, 0.7);
 		}
@@ -69,9 +70,27 @@ public class C4Draw {
 	public void getMove() {
 		//player inputs column and board[][] value accordingly changed
 		while (true) {
-			StdOut.println("Which column?");
-			int col = StdIn.readInt();
-			if (!(board[6][col].equals(" "))) {
+			int col=0; int row=0;
+			if (StdDraw.mousePressed()) {
+				double x = StdDraw.mouseX();
+				double y = StdDraw.mouseY();
+				if (x>0.15 && y<0.85 && y>0.1 && y<0.7) {
+					if (x<0.25) {	col=1;	}
+					else if (x<0.35) {	col=2;	}
+					else if (x<0.45) {	col=3;	}
+					else if (x<0.55) {	col=4;	}
+					else if (x<0.65) {	col=5;	}
+					else if (x<0.75) {	col=6;	}
+					else {	col=7;	}
+					if (y<0.2) {	row=1;	}
+					else if (y<0.3) {	row=2;	}
+					else if (y<0.4) {	row=3;	}
+					else if (y<0.5) {	row=4;	}
+					else if (y<0.6) {	row=5;	}
+					else {	row=6;	}
+					}
+			}
+			if (col>0 && !(board[6][col].equals(" "))) {
 				StdOut.println("That column is filled.");
 				StdOut.println("Lets try this again.\n");
 				continue;
@@ -90,6 +109,8 @@ public class C4Draw {
 		//vertical win check
 		if (r>=4) {
 			if (board[r][c].equals(turn) && board[r-1][c].equals(turn) && board[r-2][c].equals(turn) && board[r-3][c].equals(turn)) {
+				StdDraw.setPenColor(StdDraw.YELLOW);
+				StdDraw.line((double) (c+1)/10, (double) (r+0.75)/10, (double) (c+1)/10, (double) (r-2.75)/10);
 				return true;
 			}
 		}
@@ -97,7 +118,27 @@ public class C4Draw {
 		//horizontal win check
 		for (int col=1;col<=4;col++) {
 			if (board[r][col].equals(turn) && board[r][col+1].equals(turn) && board[r][col+2].equals(turn) && board[r][col+3].equals(turn)) {
+				StdDraw.setPenColor(StdDraw.YELLOW);
+				StdDraw.line((double) (col+0.75)/10, (r+0.5)/10, (col+4.25)/10, (r+0.5)/10);
 				return true;
+			}
+		}
+
+		// diagonal win checks
+		for (int col=1;col<=4;col++) {
+			for (int row=1;row<=3;row++) {
+				if (board[row][col].equals(turn) && board[row+1][col+1].equals(turn) && board[row+2][col+2].equals(turn) && board[row+3][col+3].equals(turn)) {
+					StdDraw.setPenColor(StdDraw.YELLOW);
+					StdDraw.line((col+0.75)/10, (row+0.25)/10, (col+4.25)/10, (row+3.75)/10);
+					return true;
+				}
+			}
+			for (int row=4;row<=6;row++) {
+				if (board[row][col].equals(turn) && board[row-1][col+1].equals(turn) && board[row-2][col+2].equals(turn) && board[row-3][col+3].equals(turn)) {
+					StdDraw.setPenColor(StdDraw.YELLOW);
+					StdDraw.line((col+0.75)/10, (row+0.75)/10, (col+4.25)/10, (row-2.75)/10);
+					return true;
+				} 
 			}
 		}
 		
@@ -112,6 +153,11 @@ public class C4Draw {
 			
 			drawBoard();
 			getMove();
+			if (checkWin()==true) {
+				drawBoard();
+				checkWin();
+				return;
+			}
 			tCount++;
 		}
 	}
